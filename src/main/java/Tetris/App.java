5dddc7e5c99a9447473261d3d115846a5a77d819
samgilmore <30483214+samgilmore@users.noreply.github.com>
2021-04-04 17:30:56 -0400

    falling objects done

--- src/main/java/Tetris/App.java
+++ src/main/java/Tetris/App.java
@@ -1,10 +1,16 @@
 package Tetris;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.scene.control.Button;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -12,7 +18,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
@@ -26,12 +31,98 @@ public class App extends Application {
     public static final int ROWS = 22;
     public static final int COLUMNS = 10;
     public static int[][] grid = new int [COLUMNS][ROWS];
+    public static Group gridLines = new Group();
     public static int score = 0;
+    private static boolean running = true;
+    public static Piece fallingPiece;
+    public static Piece nextPiece = PieceMethods.createRandomPiece();
     
-    private static Pane pane = new Pane();
+    public static Pane pane = new Pane();
+    private static Scene scene = new Scene(pane, COLUMNS * PIECE_SIZE + 250, ROWS * PIECE_SIZE);
     
     @Override
     public void start(Stage stage) {
+        
+        //Background color for grid
+        Rectangle back = new Rectangle();
+        back.setWidth(COLUMNS * PIECE_SIZE);
+        back.setHeight(ROWS * PIECE_SIZE);
+        back.setFill(Color.BLACK);
+        
+        VBox rightPanel = new VBox();
+        rightPanel.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY)));
+        rightPanel.setMinWidth(250);
+        rightPanel.setSpacing(25);
+        rightPanel.setMinHeight(ROWS * PIECE_SIZE);
+        rightPanel.setTranslateX(COLUMNS * PIECE_SIZE);
+        
+        Text scoreLabel = new Text();
+        scoreLabel.setText("SCORE");
+        scoreLabel.setTextAlignment(TextAlignment.CENTER);
+        scoreLabel.setFont(Font.font("Arial Black", 35));
+        scoreLabel.setStroke(Color.BLACK);
+        scoreLabel.setStrokeWidth(1);
+        scoreLabel.setFill(Color.GREEN);
+        HBox scoreLabelBox = new HBox();
+        scoreLabelBox.setPadding(new Insets(150,0,0,0));
+        scoreLabelBox.setAlignment(Pos.CENTER);
+        scoreLabelBox.getChildren().add(scoreLabel);
+        
+        Text scoreText = new Text();
+        scoreText.setText("0");
+        scoreText.setTextAlignment(TextAlignment.CENTER);
+        scoreText.setFont(Font.font("Arial Black", 30));
+        scoreText.setFill(Color.BLACK);
+        HBox scoreBox = new HBox();
+        scoreBox.setPadding(new Insets(0,0,90,0));
+        scoreBox.setAlignment(Pos.CENTER);
+        scoreBox.getChildren().add(scoreText);
+        
+        Button startButton = new Button("START");
+        startButton.minWidth(40);
+        startButton.setStyle("-fx-font-size: 2.5em; -fx-border-color: #111111; -fx-border-width: 2px;");
+        HBox startBox = new HBox();
+        startBox.setAlignment(Pos.CENTER);
+        startBox.getChildren().add(startButton);
+        
+        Button restartButton = new Button("RESTART");
+        restartButton.minWidth(40);
+        restartButton.setStyle("-fx-font-size: 2.5em; -fx-border-color: #111111; -fx-border-width: 2px;");
+        HBox restartBox = new HBox();
+        restartBox.setAlignment(Pos.CENTER);
+        restartBox.getChildren().add(restartButton);
+        
+        rightPanel.getChildren().addAll(scoreLabelBox, scoreBox, startBox, restartBox);
+        pane.getChildren().addAll(back,rightPanel);
+        
+        fallingPiece = nextPiece;
+        nextPiece = PieceMethods.createRandomPiece();
+        pane.getChildren().addAll(fallingPiece.getSquares().get(0),fallingPiece.getSquares().get(1),fallingPiece.getSquares().get(2),fallingPiece.getSquares().get(3)); 
+        handleKeyPress(fallingPiece);
+        
+        
+//        pane.getChildren().addAll(nextPiece.getSquares().get(0),nextPiece.getSquares().get(1),nextPiece.getSquares().get(2),nextPiece.getSquares().get(3)); 
+//        handleKeyPress(nextPiece);
+        
+        //Creating the Grid Lines
+        for (int i = 0; i < COLUMNS; i++) {
+            Line tempLine = new Line(PIECE_SIZE + (i * PIECE_SIZE), 0, PIECE_SIZE + (i * PIECE_SIZE), ROWS * PIECE_SIZE);
+            tempLine.setStrokeWidth(1);
+            tempLine.setStroke(Color.LIGHTGREY);
+            gridLines.getChildren().add(tempLine); 
+        }
+        for (int i = 0; i < ROWS; i++) {
+            Line tempLine = new Line(0, PIECE_SIZE + (i * PIECE_SIZE), COLUMNS * PIECE_SIZE, PIECE_SIZE + (i * PIECE_SIZE));
+            tempLine.setStrokeWidth(1);
+            tempLine.setStroke(Color.LIGHTGREY);
+            gridLines.getChildren().add(tempLine);
+        }
+        
+        pane.getChildren().addAll(gridLines);
+        
+        stage.setScene(scene);
+        stage.setTitle("TETRIS");
+        stage.show();
         
         //The tetris "grid" is how data will be stored if a piece has landed in a
         //certain position on the board. If a piece occupies an element in grid, it will
@@ -44,59 +135,22 @@ public class App extends Application {
             }
         }
         
-        //Background color for grid
-        Rectangle back = new Rectangle();
-        back.setWidth(COLUMNS * PIECE_SIZE);
-        back.setHeight(ROWS * PIECE_SIZE);
-        back.setFill(Color.BLACK);
-        
-        VBox rightPanel = new VBox();
-        rightPanel.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
-        rightPanel.setMinWidth(250);
-        //rightPanel.set;
-        rightPanel.setAlignment(Pos.CENTER);
-        rightPanel.setTranslateX(COLUMNS * PIECE_SIZE);
-        
-        Text scoreLabel = new Text();
-        scoreLabel.setText("SCORE");
-        scoreLabel.setTextAlignment(TextAlignment.CENTER);
-        scoreLabel.setFont(Font.font("Arial Black", 35));
-        scoreLabel.setFill(Color.GREEN);
-        HBox scoreBox = new HBox();
-        scoreBox.setAlignment(Pos.CENTER);
-        scoreBox.getChildren().add(scoreLabel);
-       // scoreLabel.setY(ROWS * PIECE_SIZE / 3);
-        
-        rightPanel.getChildren().addAll(scoreBox);
-        pane.getChildren().addAll(back,rightPanel);
-        
-        Piece nextPiece = PieceMethods.createRandomPiece();
-        pane.getChildren().addAll(nextPiece.getSquares().get(0),nextPiece.getSquares().get(1),nextPiece.getSquares().get(2),nextPiece.getSquares().get(3));
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(() -> {
+                    if (running == true) {
+                        score++;
+                        scoreText.setText(Integer.toString(score));
+                        PieceMethods.moveDown(fallingPiece);
+                    }
+                });
+            }
+        };
         
         
-        
-        //Creating the Grid Lines
-        Group grid = new Group();
-        for (int i = 0; i < COLUMNS; i++) {
-            Line tempLine = new Line(PIECE_SIZE + (i * PIECE_SIZE), 0, PIECE_SIZE + (i * PIECE_SIZE), ROWS * PIECE_SIZE);
-            tempLine.setStrokeWidth(1);
-            tempLine.setStroke(Color.LIGHTGREY);
-            grid.getChildren().add(tempLine); 
-        }
-        for (int i = 0; i < ROWS; i++) {
-            Line tempLine = new Line(0, PIECE_SIZE + (i * PIECE_SIZE), COLUMNS * PIECE_SIZE, PIECE_SIZE + (i * PIECE_SIZE));
-            tempLine.setStrokeWidth(1);
-            tempLine.setStroke(Color.LIGHTGREY);
-            grid.getChildren().add(tempLine);
-        }
-        
-        
-        
-        pane.getChildren().addAll(grid);
-        Scene scene = new Scene(pane, COLUMNS * PIECE_SIZE + 250, ROWS * PIECE_SIZE);
-        stage.setScene(scene);
-        stage.setTitle("TETRIS");
-        stage.show();
+        Timer timer = new Timer();
+        timer.schedule(timerTask, 0, 1000);
         
     }
 
@@ -104,4 +158,16 @@ public class App extends Application {
         launch();
     }
 
+    public static void handleKeyPress (Piece piece) {
+                scene.setOnKeyPressed((KeyEvent t) -> {
+                    if (t.getCode() == KeyCode.A) {
+                        PieceMethods.moveLeft(piece);
+                    } else if (t.getCode() == KeyCode.D) {
+                        PieceMethods.moveRight(piece);
+                    } else if (t.getCode() == KeyCode.S) {
+                        PieceMethods.moveDown(piece);
+                    }
+                });
+    }
+    
 }
--- src/main/java/Tetris/PieceMethods.java
+++ src/main/java/Tetris/PieceMethods.java
@@ -10,6 +10,7 @@ public class PieceMethods {
     public static final int COLUMNS = App.COLUMNS;
     public static int[][] grid = App.grid;
     private static final int BOARD_WIDTH = COLUMNS * PIECE_SIZE;
+    private static final int BOARD_HEIGHT = ROWS * PIECE_SIZE;
     
     public static Piece createRandomPiece() {
        Random rand = new Random();
@@ -19,80 +20,75 @@ public class PieceMethods {
        
        //A tetris piece only has four squares. These squares spawn in specific places
        //at the top of the board.
-       Square one = new Square();
-       Square two = new Square();
-       Square three = new Square();
-       Square four = new Square();
+       squares.add(new Square());
+       squares.add(new Square());
+       squares.add(new Square());
+       squares.add(new Square());
        
        //Piece spawn positions from: https://harddrop.com/wiki/Spawn_Location#:~:text=Tetris%20at%20tetris.com%20spawns,existing%20blocks%20on%20the%20field.
-
+       //Each square must be set in a specific spot when spawned in
        if (randomPiece == 0) {
            id = "I";
-           one.setX(BOARD_WIDTH / 2 - (PIECE_SIZE * 2));
-           two.setX(BOARD_WIDTH / 2 - PIECE_SIZE);
-           three.setX(BOARD_WIDTH / 2);
-           four.setX(BOARD_WIDTH / 2 + PIECE_SIZE);
-           one.setY(PIECE_SIZE);
-           two.setY(PIECE_SIZE);
-           three.setY(PIECE_SIZE);
-           four.setY(PIECE_SIZE);
+           squares.get(0).setX(BOARD_WIDTH / 2 - (PIECE_SIZE * 2));
+           squares.get(1).setX(BOARD_WIDTH / 2 - PIECE_SIZE);
+           squares.get(2).setX(BOARD_WIDTH / 2);
+           squares.get(3).setX(BOARD_WIDTH / 2 + PIECE_SIZE);
+           squares.get(0).setY(PIECE_SIZE);
+           squares.get(1).setY(PIECE_SIZE);
+           squares.get(2).setY(PIECE_SIZE);
+           squares.get(3).setY(PIECE_SIZE);
        } else if (randomPiece == 1) {
            id = "J";
-           one.setX(BOARD_WIDTH / 2 - (PIECE_SIZE * 2));
-           two.setX(BOARD_WIDTH / 2 - (PIECE_SIZE * 2));
-           three.setX(BOARD_WIDTH / 2 - PIECE_SIZE);
-           four.setX(BOARD_WIDTH / 2);
-           two.setY(PIECE_SIZE);
-           three.setY(PIECE_SIZE);
-           four.setY(PIECE_SIZE);
+           squares.get(0).setX(BOARD_WIDTH / 2 - (PIECE_SIZE * 2));
+           squares.get(1).setX(BOARD_WIDTH / 2 - (PIECE_SIZE * 2));
+           squares.get(2).setX(BOARD_WIDTH / 2 - PIECE_SIZE);
+           squares.get(3).setX(BOARD_WIDTH / 2);
+           squares.get(1).setY(PIECE_SIZE);
+           squares.get(2).setY(PIECE_SIZE);
+           squares.get(3).setY(PIECE_SIZE);
        } else if (randomPiece == 2) {
            id = "L";
-           one.setX(BOARD_WIDTH / 2 - (PIECE_SIZE * 2));
-           two.setX(BOARD_WIDTH / 2 - PIECE_SIZE);
-           three.setX(BOARD_WIDTH / 2);
-           four.setX(BOARD_WIDTH / 2);
-           one.setY(PIECE_SIZE);
-           two.setY(PIECE_SIZE);
-           three.setY(PIECE_SIZE);
+           squares.get(0).setX(BOARD_WIDTH / 2 - (PIECE_SIZE * 2));
+           squares.get(1).setX(BOARD_WIDTH / 2 - PIECE_SIZE);
+           squares.get(2).setX(BOARD_WIDTH / 2);
+           squares.get(3).setX(BOARD_WIDTH / 2);
+           squares.get(0).setY(PIECE_SIZE);
+           squares.get(1).setY(PIECE_SIZE);
+           squares.get(2).setY(PIECE_SIZE);
        } else if (randomPiece == 3) {
            id = "O";
-           one.setX(BOARD_WIDTH / 2 - PIECE_SIZE);
-           two.setX(BOARD_WIDTH / 2);
-           three.setX(BOARD_WIDTH / 2 - PIECE_SIZE);
-           four.setX(BOARD_WIDTH / 2);
-           three.setY(PIECE_SIZE);
-           four.setY(PIECE_SIZE);
+           squares.get(0).setX(BOARD_WIDTH / 2 - PIECE_SIZE);
+           squares.get(1).setX(BOARD_WIDTH / 2);
+           squares.get(2).setX(BOARD_WIDTH / 2 - PIECE_SIZE);
+           squares.get(3).setX(BOARD_WIDTH / 2);
+           squares.get(2).setY(PIECE_SIZE);
+           squares.get(3).setY(PIECE_SIZE);
        } else if (randomPiece == 4) {
            id = "S";
-           one.setX(BOARD_WIDTH / 2 - (PIECE_SIZE * 2));
-           two.setX(BOARD_WIDTH / 2 - PIECE_SIZE);
-           three.setX(BOARD_WIDTH / 2 - PIECE_SIZE);
-           four.setX(BOARD_WIDTH / 2);
-           one.setY(PIECE_SIZE);
-           two.setY(PIECE_SIZE);
+           squares.get(0).setX(BOARD_WIDTH / 2 - (PIECE_SIZE * 2));
+           squares.get(1).setX(BOARD_WIDTH / 2 - PIECE_SIZE);
+           squares.get(2).setX(BOARD_WIDTH / 2 - PIECE_SIZE);
+           squares.get(3).setX(BOARD_WIDTH / 2);
+           squares.get(0).setY(PIECE_SIZE);
+           squares.get(1).setY(PIECE_SIZE);
        } else if (randomPiece == 5) {
            id = "T";
-           one.setX(BOARD_WIDTH / 2 - (PIECE_SIZE * 2));
-           two.setX(BOARD_WIDTH / 2 - PIECE_SIZE);
-           three.setX(BOARD_WIDTH / 2 - PIECE_SIZE);
-           four.setX(BOARD_WIDTH / 2);
-           one.setY(PIECE_SIZE);
-           two.setY(PIECE_SIZE);
-           four.setY(PIECE_SIZE);
+           squares.get(0).setX(BOARD_WIDTH / 2 - (PIECE_SIZE * 2));
+           squares.get(1).setX(BOARD_WIDTH / 2 - PIECE_SIZE);
+           squares.get(2).setX(BOARD_WIDTH / 2 - PIECE_SIZE);
+           squares.get(3).setX(BOARD_WIDTH / 2);
+           squares.get(0).setY(PIECE_SIZE);
+           squares.get(1).setY(PIECE_SIZE);
+           squares.get(3).setY(PIECE_SIZE);
        } else {
            id = "Z";
-           one.setX(BOARD_WIDTH / 2 - (PIECE_SIZE * 2));
-           two.setX(BOARD_WIDTH / 2 - PIECE_SIZE);
-           three.setX(BOARD_WIDTH / 2 - PIECE_SIZE);
-           four.setX(BOARD_WIDTH / 2);
-           three.setY(PIECE_SIZE);
-           four.setY(PIECE_SIZE);
+           squares.get(0).setX(BOARD_WIDTH / 2 - (PIECE_SIZE * 2));
+           squares.get(1).setX(BOARD_WIDTH / 2 - PIECE_SIZE);
+           squares.get(2).setX(BOARD_WIDTH / 2 - PIECE_SIZE);
+           squares.get(3).setX(BOARD_WIDTH / 2);
+           squares.get(2).setY(PIECE_SIZE);
+           squares.get(3).setY(PIECE_SIZE);
        }
-       
-       squares.add(one);
-       squares.add(two);
-       squares.add(three);
-       squares.add(four);
        return new Piece(squares,id);
     }
 
@@ -111,15 +107,14 @@ public class PieceMethods {
             //Next, find the value of the potential location in the grid array for each square in the piece.
             //If the location is empty for every square, aka == 0, then move piece visually.
             //We don't need to update grid array yet because the piece hasn't landed.
-            int potentialOne = grid[(int) piece.getSquares().get(0).getX() / PIECE_SIZE - 1][(int) piece.getSquares().get(0).getY() / PIECE_SIZE];
-            int potentialTwo = grid[(int) piece.getSquares().get(1).getX() / PIECE_SIZE - 1][(int) piece.getSquares().get(1).getY() / PIECE_SIZE];
-            int potentialThree = grid[(int) piece.getSquares().get(2).getX() / PIECE_SIZE - 1][(int) piece.getSquares().get(2).getY() / PIECE_SIZE];
-            int potentialFour = grid[(int) piece.getSquares().get(3).getX() / PIECE_SIZE - 1][(int) piece.getSquares().get(3).getY() / PIECE_SIZE];
-            if (potentialOne == 0 && potentialTwo == 0 && potentialThree == 0 && potentialFour == 0) {
-                piece.getSquares().get(0).setX(piece.getSquares().get(0).getX() - PIECE_SIZE);
-                piece.getSquares().get(1).setX(piece.getSquares().get(1).getX() - PIECE_SIZE);
-                piece.getSquares().get(2).setX(piece.getSquares().get(2).getX() - PIECE_SIZE);
-                piece.getSquares().get(3).setX(piece.getSquares().get(3).getX() - PIECE_SIZE);
+            int potentials[] = new int[4];
+            for (int i=0; i < 4; i++) {
+                potentials[i] = grid[(int) piece.getSquares().get(i).getX() / PIECE_SIZE - 1][(int) piece.getSquares().get(i).getY() / PIECE_SIZE];
+            }
+            if (potentials[0] == 0 && potentials[1] == 0 && potentials[2] == 0 && potentials[3] == 0) {
+                for (int i=0; i < piece.getSquares().size(); i++) {
+                    piece.getSquares().get(i).setX(piece.getSquares().get(i).getX() - PIECE_SIZE);
+                }
             }
         }
     }
@@ -133,18 +128,66 @@ public class PieceMethods {
             //Next, find the value of the potential location in the grid array for each square in the piece.
             //If the location is empty for every square, aka == 0, then move piece visually.
             //We don't need to update grid array yet because the piece hasn't landed.
-            int potentialOne = grid[(int) piece.getSquares().get(0).getX() / PIECE_SIZE + 1][(int) piece.getSquares().get(0).getY() / PIECE_SIZE];
-            int potentialTwo = grid[(int) piece.getSquares().get(1).getX() / PIECE_SIZE + 1][(int) piece.getSquares().get(1).getY() / PIECE_SIZE];
-            int potentialThree = grid[(int) piece.getSquares().get(2).getX() / PIECE_SIZE + 1][(int) piece.getSquares().get(2).getY() / PIECE_SIZE];
-            int potentialFour = grid[(int) piece.getSquares().get(3).getX() / PIECE_SIZE + 1][(int) piece.getSquares().get(3).getY() / PIECE_SIZE];
-            if (potentialOne == 0 && potentialTwo == 0 && potentialThree == 0 && potentialFour == 0) {
-                piece.getSquares().get(0).setX(piece.getSquares().get(0).getX() + PIECE_SIZE);
-                piece.getSquares().get(1).setX(piece.getSquares().get(1).getX() + PIECE_SIZE);
-                piece.getSquares().get(2).setX(piece.getSquares().get(2).getX() + PIECE_SIZE);
-                piece.getSquares().get(3).setX(piece.getSquares().get(3).getX() + PIECE_SIZE);
+            int potentials[] = new int[4];
+            for (int i=0; i < 4; i++) {
+                potentials[i] = grid[(int) piece.getSquares().get(i).getX() / PIECE_SIZE + 1][(int) piece.getSquares().get(i).getY() / PIECE_SIZE];
+            }
+            if (potentials[0] == 0 && potentials[1] == 0 && potentials[2] == 0 && potentials[3] == 0) {
+                for (int i=0; i < piece.getSquares().size(); i++) {
+                    piece.getSquares().get(i).setX(piece.getSquares().get(i).getX() + PIECE_SIZE);
+                }
             }
         }
     }
     
+    public static void moveDown(Piece piece) {
+        if (piece.getSquares().get(0).getY() + PIECE_SIZE <= BOARD_HEIGHT - PIECE_SIZE &&
+                piece.getSquares().get(1).getY() + PIECE_SIZE <= BOARD_HEIGHT - PIECE_SIZE &&
+                piece.getSquares().get(2).getY() + PIECE_SIZE <= BOARD_HEIGHT - PIECE_SIZE &&
+                piece.getSquares().get(3).getY() + PIECE_SIZE <= BOARD_HEIGHT - PIECE_SIZE) {
+           
+            //Next, find the value of the potential location in the grid array for each square in the piece.
+            //If the location is empty for every square, aka == 0, then move piece visually.
+            //We don't need to update grid array yet because the piece hasn't landed.
+            
+            int potentialOne = grid[(int) piece.getSquares().get(0).getX() / PIECE_SIZE ][(int) piece.getSquares().get(0).getY() / PIECE_SIZE];
+            int potentialTwo = grid[(int) piece.getSquares().get(1).getX() / PIECE_SIZE ][(int) piece.getSquares().get(1).getY() / PIECE_SIZE];
+            int potentialThree = grid[(int) piece.getSquares().get(2).getX() / PIECE_SIZE ][(int) piece.getSquares().get(2).getY() / PIECE_SIZE];
+            int potentialFour = grid[(int) piece.getSquares().get(3).getX() / PIECE_SIZE ][(int) piece.getSquares().get(3).getY() / PIECE_SIZE];
+            
+            if (potentialOne == 0 && potentialTwo == 0 && potentialThree == 0 && potentialFour == 0) {
+                piece.getSquares().get(0).setY(piece.getSquares().get(0).getY() + PIECE_SIZE);
+                piece.getSquares().get(1).setY(piece.getSquares().get(1).getY() + PIECE_SIZE);
+                piece.getSquares().get(2).setY(piece.getSquares().get(2).getY() + PIECE_SIZE);
+                piece.getSquares().get(3).setY(piece.getSquares().get(3).getY() + PIECE_SIZE);
+            }  
+        }
+        // generate a new piece once the current piece has landed
+        
+        if (piece.getSquares().get(0).getY() == BOARD_HEIGHT - PIECE_SIZE || piece.getSquares().get(1).getY() == BOARD_HEIGHT - PIECE_SIZE ||
+                piece.getSquares().get(2).getY() == BOARD_HEIGHT - PIECE_SIZE || piece.getSquares().get(3).getY() == BOARD_HEIGHT - PIECE_SIZE || isPieceBelow(piece)) {
+            for (int i = 0; i < 4; i++) {
+                grid[(int) piece.getSquares().get(i).getX() / PIECE_SIZE][(int) piece.getSquares().get(i).getY() / PIECE_SIZE] = 1;
+            }
+            //Since piece has landed, we need to check if a row has been completed
+            
+            //Next, we need to generate a new piece now that the old one has landed as well as take control of the new falling piece.
+            App.fallingPiece = App.nextPiece;
+            App.nextPiece = createRandomPiece();
+            App.pane.getChildren().addAll(App.fallingPiece.getSquares().get(0),App.fallingPiece.getSquares().get(1),App.fallingPiece.getSquares().get(2),App.fallingPiece.getSquares().get(3)); 
+            App.gridLines.toFront();
+            App.handleKeyPress(App.fallingPiece);
+        }
+        
+    }
     
+    private static boolean isPieceBelow(Piece piece) {
+        boolean isPieceBelow = false;
+        for (int i = 0; i < piece.getSquares().size(); i++) {
+            if (App.grid[(int) piece.getSquares().get(i).getX() / PIECE_SIZE][(int) piece.getSquares().get(i).getY() / PIECE_SIZE + 1] == 1) {
+                isPieceBelow = true;
+            }
+        }
+        return isPieceBelow;
+    }
 }
